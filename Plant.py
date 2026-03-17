d8fab9d12dbf97ff77ded05bdbeff7d2d9d04b0c
NeuralEngines <55753233+andrsngm@users.noreply.github.com>
2026-03-17 14:37:27 -0400

    Update Plant.py

--- Plant.py
+++ Plant.py
@@ -199,4 +199,4 @@ def eliminar_registro():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=8080)
